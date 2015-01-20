bce3f07b6bda53b9854dcbb34942ac57bb28c167
Mike Cardillo <mcardillo55@gmail.com>
2015-01-20 13:21:21 -0800

    style changes

--- unfriended-flask/app.py
+++ unfriended-flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from config import APP_SECRET, SQLALCHEMY_DATABASE_URI
 from database import init_db
 
@@ -12,9 +13,11 @@ BLUEPRINT_MODULES = [facebook,
 app = Flask(__name__)
 app.secret_key = APP_SECRET
 init_db(app)
+Bootstrap(app)
 
 for module in BLUEPRINT_MODULES:
     app.register_blueprint(module)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
