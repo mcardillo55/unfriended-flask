2087fe6f94ed27873772a11a9ba5adc7981c0529
Mike Cardillo <mcardillo55@gmail.com>
2015-12-04 05:08:08 -0800

    App was crashing at system boot because MySQL was not started yet. Added a wait for the MySQL server to start before proceeding with db activity

--- unfriended-flask/database.py
+++ unfriended-flask/database.py
@@ -1,5 +1,7 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import SQLALCHEMY_DATABASE_URI
+import os.path
+import time
 
 db = SQLAlchemy()
 
@@ -8,5 +10,7 @@ def init_db(app):
     import unfriended.models
     app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
     db.init_app(app)
+    while not os.path.exists('/var/run/mysqld/mysqld.sock'):
+        time.sleep(5)
     with app.app_context():
         db.create_all()
